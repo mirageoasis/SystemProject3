74c1ca42411bcd632f8f52514074c69f9b9b54c5
kimhw0820 <kimhw0820@naver.com>
2022-06-24 18:16:02 +0900

    realloc

--- mm.c
+++ mm.c
@@ -37,30 +37,30 @@ team_t team = {
 
 /* $begin mallocmacros */
 /* Basic constants and macros */
-#define WSIZE 4 /* Word and header/footer size (bytes) */            // line:vm:mm:beginconst
-#define DSIZE 8                                                      /* Doubleword size (bytes) */
-#define CHUNKSIZE (1 << 12) /* Extend heap by this amount (bytes) */ // line:vm:mm:endconst
+#define WSIZE 4 /* Word and header/footer size (bytes) */            
+#define DSIZE 8                                                      
+#define CHUNKSIZE (1 << 12) /* Extend heap by this amount (bytes) */
 
 #define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
-#define PACK(size, alloc) ((size) | (alloc)) // line:vm:mm:pack
+#define PACK(size, alloc) ((size) | (alloc)) 
 
 /* Read and write a word at address p */
-#define GET(p) (*(unsigned int *)(p))              // line:vm:mm:get
-#define PUT(p, val) (*(unsigned int *)(p) = (val)) // line:vm:mm:put
+#define GET(p) (*(unsigned int *)(p))              
+#define PUT(p, val) (*(unsigned int *)(p) = (val)) 
 
 /* Read the size and allocated fields from address p */
-#define GET_SIZE(p) (GET(p) & ~0x7) // line:vm:mm:getsize
-#define GET_ALLOC(p) (GET(p) & 0x1) // line:vm:mm:getalloc
+#define GET_SIZE(p) (GET(p) & ~0x7) 
+#define GET_ALLOC(p) (GET(p) & 0x1) 
 
 /* Given block ptr bp, compute address of its header and footer */
-#define HDRP(bp) ((char *)(bp)-WSIZE)                        // line:vm:mm:hdrp
-#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) // line:vm:mm:ftrp
+#define HDRP(bp) ((char *)(bp)-WSIZE)                        
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE) 
 
 /* Given block ptr bp, compute address of next and previous blocks */
-#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE))) // line:vm:mm:nextblkp
-#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))   // line:vm:mm:prevblkp
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp)-WSIZE))) 
+#define PREV_BLKP(bp) ((char *)(bp)-GET_SIZE(((char *)(bp)-DSIZE)))   
 /* $end mallocmacros */
 
 /* Global variables */
@@ -74,10 +74,7 @@ static void *extend_heap(size_t words);
 static void place(void *bp, size_t asize);
 static void *find_fit(size_t asize);
 static void *coalesce(void *bp);
-/*
- * mm_init - Initialize the memory manager
- */
-/* $begin mminit */
+
 int mm_init(void)
 {
     /* Create the initial empty heap */
@@ -100,71 +97,47 @@ int mm_init(void)
         return -1;
     return 0;
 }
-/* $end mminit */
 
-/*
- * mm_malloc - Allocate a block with at least size bytes of payload
- */
-/* $begin mmmalloc */
+
+
 void *mm_malloc(size_t size)
 {
     size_t asize;      /* Adjusted block size */
     size_t extendsize; /* Amount to extend heap if no fit */
     char *bp;
 
-    /* $end mmmalloc */
-    if (heap_listp == 0)
-    {
-        mm_init();
-    }
-    /* $begin mmmalloc */
     /* Ignore spurious requests */
     if (size == 0)
         return NULL;
 
     /* Adjust block size to include overhead and alignment reqs. */
-    if (size <= DSIZE)     // line:vm:mm:sizeadjust1
-        asize = 2 * DSIZE; // line:vm:mm:sizeadjust2
+    if (size <= DSIZE)     
+        asize = 2 * DSIZE;
     else
         asize = DSIZE * ((size + (DSIZE) + (DSIZE - 1)) / DSIZE); // line:vm:mm:sizeadjust3
 
     /* Search the free list for a fit */
     if ((bp = find_fit(asize)) != NULL)
-    {                     // line:vm:mm:findfitcall
-        place(bp, asize); // line:vm:mm:findfitplace
+    {                     
+        place(bp, asize); 
         return bp;
     }
 
     /* No fit found. Get more memory and place the block */
     extendsize = MAX(asize, CHUNKSIZE); // line:vm:mm:growheap1
     if ((bp = extend_heap(extendsize / WSIZE)) == NULL)
-        return NULL;  // line:vm:mm:growheap2
-    place(bp, asize); // line:vm:mm:growheap3
+        return NULL;  
+    place(bp, asize); 
 #ifdef TEST
     mm_checkheap(0);
 #endif
     return bp;
 }
-/* $end mmmalloc */
 
-/*
- * mm_free - Free a block
- */
-/* $begin mmfree */
+
 void mm_free(void *bp)
 {
-    /* $end mmfree */
-    if (bp == 0)
-        return;
-
-    /* $begin mmfree */
     size_t size = GET_SIZE(HDRP(bp));
-    /* $end mmfree */
-    if (heap_listp == 0)
-    {
-        mm_init();
-    }
-    /* $begin mmfree */
 
     PUT(HDRP(bp), PACK(size, 0));
     PUT(FTRP(bp), PACK(size, 0));
@@ -174,11 +147,6 @@ void mm_free(void *bp)
 #endif
 }
 
-/* $end mmfree */
-/*
- * coalesce - Boundary tag coalescing. Return ptr to coalesced block
- */
-/* $begin mmfree */
 static void *coalesce(void *bp)
 {
     size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
@@ -213,53 +181,28 @@ static void *coalesce(void *bp)
         PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
         bp = PREV_BLKP(bp);
     }
-/* $end mmfree */
-#ifdef NEXT_FIT
-    /* Make sure the allocPointer isn't pointing into the free block */
-    /* that we just coalesced */
 
+#ifdef NEXT_FIT
     if ((allocPointer > (char *)bp))
         allocPointer = bp;
 #endif
-    /* $begin mmfree */
     return bp;
 }
-/* $end mmfree */
 
-/*
- * mm_realloc - Naive implementation of realloc
- */
+
 void *mm_realloc(void *ptr, size_t size)
 {
-    size_t oldsize;
+    size_t prevsize;
     void *newptr;
-
-    /* If size == 0 then this is just free, and we return NULL. */
-    if (size == 0)
-    {
-        mm_free(ptr);
+    /*malloc 에 오류가 발생할 때 realloc이 fail 했음을 알림*/
+    if(!(newptr = mm_malloc(size)))
         return 0;
-    }
-
-    /* If oldptr is NULL, then this is just malloc. */
-    if (ptr == NULL)
-    {
-        return mm_malloc(size);
-    }
-
-    newptr = mm_malloc(size);
-
-    /* If realloc() fails the original block is left untouched  */
-    if (!newptr)
-    {
-        return 0;
-    }
 
     /* Copy the old data. */
-    oldsize = GET_SIZE(HDRP(ptr));
-    if (size < oldsize)
-        oldsize = size;
-    memcpy(newptr, ptr, oldsize);
+    prevsize = GET_SIZE(HDRP(ptr));
+    if (size < prevsize)
+        prevsize = size;
+    memcpy(newptr, ptr, prevsize);
 
     /* Free the old block. */
     mm_free(ptr);
